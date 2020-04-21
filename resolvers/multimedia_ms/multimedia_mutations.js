e456ee4902c866b9f29bd47afaa34ee76828da1a
daandradec <daandradec@unal.edu.co>
2020-04-20 22:27:59 -0500

    clean sintax error

--- resolvers/multimedia_ms/multimedia_mutations.js
+++ resolvers/multimedia_ms/multimedia_mutations.js
@@ -2,12 +2,12 @@
 import axios from 'axios';
 /* STORE FILE IMAGE BY ID AND TYPE */
 export const storeFile = async (_, {id, type, file}, ctx) => {
-    return await axios.post(`http://${ctx.MULTIMEDIA_URL}}:${ctx.MULTIMEDIA_PORT}/${ctx.MULTIMEDIA_BASE_ENDPOINT}/${id}/${type}`, {mediafile:file}).then(res => res.data);
+    return await axios.post(`http://${ctx.MULTIMEDIA_URL}:${ctx.MULTIMEDIA_PORT}/${ctx.MULTIMEDIA_BASE_ENDPOINT}/${id}/${type}`, {mediafile:file}).then(res => res.data);
 }
 
 /* UPDATE FILE IMAGE BY ID AND TYPE */
 export const updateFile = async (_, {id, type, file}, ctx) => {
-    return await axios.put(`http://${ctx.MULTIMEDIA_URL}}:${ctx.MULTIMEDIA_PORT}/${ctx.MULTIMEDIA_BASE_ENDPOINT}/${id}/${type}`, {mediafile:file}).then(res => res.data)
+    return await axios.put(`http://${ctx.MULTIMEDIA_URL}:${ctx.MULTIMEDIA_PORT}/${ctx.MULTIMEDIA_BASE_ENDPOINT}/${id}/${type}`, {mediafile:file}).then(res => res.data)
 }
 
 /* DELETE FILE IMAGE BY ID AND TYPE */
